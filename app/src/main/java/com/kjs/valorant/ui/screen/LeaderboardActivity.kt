4104b3b40342f834d12b94a162553445b02d9804
jayden.moon <jayden.moon@kakaocorp.com>
2023-06-01 22:00:52 +0900

    리더보드 화면 추가

---
+++ app/src/main/java/com/kjs/valorant/ui/screen/LeaderboardActivity.kt
@@ -0,0 +1,40 @@
+package com.kjs.valorant.ui.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kjs.valorant.ui.theme.ValorantTheme
+
+class LeaderboardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ValorantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting2("Leaderboard!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    ValorantTheme {
+        Greeting2("Android")
+    }
+}
