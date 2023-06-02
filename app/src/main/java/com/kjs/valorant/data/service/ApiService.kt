9f9a5b614e583252be0ce97a4daf10e882042890
jayden.moon <jayden.moon@kakaocorp.com>
2023-06-02 21:22:05 +0900

    [VT-3] API 연동

--- app/src/main/java/com/kjs/valorant/data/service/ApiService.kt
+++ app/src/main/java/com/kjs/valorant/data/service/ApiService.kt
@@ -3,6 +3,8 @@ package com.kjs.valorant.data.service
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ApiService {
@@ -13,9 +15,15 @@ object ApiService {
     }
 
     val valorant: ValorantApiService by lazy {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(ValorantInterceptor())
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS))
+            .build()
+
         Retrofit.Builder()
             .baseUrl("https://kr.api.riotgames.com")
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .client(client)
             .build()
             .create(ValorantApiService::class.java)
     }
